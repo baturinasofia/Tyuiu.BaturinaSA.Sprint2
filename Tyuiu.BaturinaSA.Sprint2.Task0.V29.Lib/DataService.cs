98b0abd5c80bbcf5519a61ca87a64a757dab4220
baturinasofia <sofi1608@bk.ru>
2025-10-13 02:11:58 +0500

    Add project files.

---
+++ Tyuiu.BaturinaSA.Sprint2.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BaturinaSA.Sprint2.Task0.V29.Lib
+{
+    public class DataService : ISprint2Task0V29
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x + 135 == y;
+            res[1] = x != 105;
+            res[2] = x < y;
+            res[3] = x + 720 > y;
+            res[4] = x >= y;
+            res[5] = x + 687 <= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint2.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BaturinaSA.Sprint2.Task0.V29.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint2.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 105;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, false, true, true, false, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
