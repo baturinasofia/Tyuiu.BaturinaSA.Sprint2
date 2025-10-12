b7977b6538c0ae67ed5dcda755356413e4699780
baturinasofia <sofi1608@bk.ru>
2025-10-13 02:33:45 +0500

    Task4

---
+++ Tyuiu.BaturinaSA.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BaturinaSA.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 3.125;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 625;
+            double res = ds.Calculate(x, y);
+            double wait = -0.234;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BaturinaSA.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint2.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Батурина С. А. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Батурина Софья Алексеевна | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменных х,у с клавиатуры. Ответ округлите до 3 знаков после запятой  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите X: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите Y: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
